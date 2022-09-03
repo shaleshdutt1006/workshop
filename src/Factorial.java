1e1461101d2fc2f312251c092d4d4d1dfd53e510
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-03 09:57:44 +0530

    Adding : Calculating the factorial number

--- src/Factorial.java
+++ src/Factorial.java
@@ -8,7 +8,7 @@ public class Factorial {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter number");
         int number = sc.nextInt();
-        for (int i=1 ; i<number ; i++){
+        for (int i=1 ; i<=number ; i++){
             factorial = factorial*i;
         }
         System.out.println(factorial);
