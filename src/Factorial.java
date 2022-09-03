89aac202fd21576b1528850b0e2fc31b74929661
shalesh1006 <shaleshdutt1006@gmail.com>
2022-09-03 09:38:18 +0530

    Modified : Adding the factorial number program

---
+++ src/Factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        int factorial=1;
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int i=1 ; i<number ; i++){
+            factorial = factorial*i;
+        }
+        System.out.println(factorial);
+    }
+}
